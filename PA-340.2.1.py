a1986ee4415eb5b285926a1d80e4ad56f496d7a9
Nasseima <lindsaynasseima@gmail.com>
2024-10-08 12:43:34 -0400

    All done

---
+++ PA-340.2.1.py
@@ -0,0 +1,46 @@
+# Problem 1
+x = 20
+y = 30
+sum = x + y
+print ("The sum is",sum)
+
+# Problem 2
+a = 30.45
+b = 10.60
+sum = a + b
+print ("The sum is",sum)
+
+# Problem 3
+num1 = 5
+num2 = 4.50
+sum = num1 + num2
+print ("The sum is",sum)
+# The variable type is a float
+
+# Problem 4
+x = 20
+y = 40
+div = y/x
+print ("The result is",div)
+
+# Problem 5
+num1 = 5
+num2 = 25
+div = num2/num1
+print("The result is",div)
+
+# Problem 6 & 7
+coffee = 6.95
+cappuccino = 10.25
+espresso = 12.10
+green_tea = 5.80
+
+subtotal = (3 * coffee) + (4 * cappuccino) + (2 * espresso)
+Sales_tax = 0.08 * subtotal
+totalSale = Sales_tax + subtotal
+print("The total sale is",totalSale)
+
+# Problem 8 
+radius = 63
+area = 3.14 * (radius * radius)
+print("The area is",area)
